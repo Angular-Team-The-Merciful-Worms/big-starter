00061d3e34a5ba8c212413c72ca8cbabc08b1915
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-09-09 23:29:46 +0300

    changed project-list.component to work with data bindings

--- src/app/project/projects-list/projects-list.component.ts
+++ src/app/project/projects-list/projects-list.component.ts
@@ -1,14 +1,37 @@
+import { ProjectService } from './../project-service';
+import { IProject } from './../project';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './projects-list.component.html',
   styleUrls: ['./projects-list.component.css']
 })
 export class ProjectsListComponent implements OnInit {
+  projects: IProject[] = [];
+  filteredProjects: IProject[];
+  gainedPercent: number = 0;
+  defaultImageUrl: string = '../../../assets/no-image.jpg';
+  errorMessage: string;
+  categoryFilter: string;
+  _listFilter: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  // to use for name filter
+  get listFilter(): string {
+    return this._listFilter;
   }
 
+  set listFilter(value: string) {
+    this._listFilter = value;
+  }
+
+  constructor(private _route: ActivatedRoute,
+    private _router: Router, private _projectService: ProjectService) { }
+
+  ngOnInit(): void {
+    this.categoryFilter = this._route.snapshot.paramMap.get('category');
+    this._projectService.getProjectsByCategory(this.categoryFilter)
+      .subscribe(projects => this.projects = projects,
+      error => this.errorMessage = <any>error);
+  }
 }
