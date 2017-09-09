6b222ff7ee24d311ca2d5eacbd6a5d57c153f66a
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-09-09 20:15:48 +0300

    added interface for project

---
+++ src/app/project/project.ts
@@ -0,0 +1,13 @@
+export interface IProject {
+    projectId: number;
+    projectName: string;
+    authorName: string;
+    description: string;
+    category: string;
+    location: number;
+    dateCreated: number;
+    votes: string;
+    accumulatedFunds: number;
+    targetFunds: number;
+    imageUrl: string;
+}
