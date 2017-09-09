e08218d10138e400a5ec92c3288823427aae719c
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-09-09 15:10:32 +0300

    added ProjectItemComponent to project module uncommented and used route for projects/:id

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -3,20 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { ProjectsListComponent } from './projects-list/projects-list.component';
+import { ProjectItemComponent } from './project-item/project-item.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path: 'projects', component: ProjectsListComponent },
-      // {
-      //   path: 'projects/:id',
-      //   canActivate: [ProjectGuardService],
-      //   component: ProjectDetailComponent
-      // }
+      {
+        path: 'projects/:id',
+        //canActivate: [ProjectGuardService],
+        component: ProjectItemComponent
+      }
     ])
   ],
-  declarations: [ProjectsListComponent],
+  declarations: [ProjectsListComponent, ProjectItemComponent],
   providers: []
 })
 export class ProjectModule { }
