2b4b2aa57807e140af328b3da5fca2413a9ef3cd
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-09-09 23:27:47 +0300

    changed project item component to work with data binding

--- src/app/project/project-item/project-item.component.ts
+++ src/app/project/project-item/project-item.component.ts
@@ -1,4 +1,7 @@
+import { ProjectService } from './../project-service';
+import { IProject } from './../project';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-item',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-item.component.css']
 })
 export class ProjectItemComponent implements OnInit {
+  errorMessage: string;
+  project: IProject;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _route: ActivatedRoute,
+    private _router: Router,
+    private _projectService: ProjectService) {
   }
 
+  ngOnInit() {
+    const id = +this._route.snapshot.paramMap.get('id');
+    console.log(id);
+    this.getProject(id);
+  }
+
+  getProject(id: number) {
+    this._projectService.getProject(id).subscribe(
+      project => this.project = project,
+      error => this.errorMessage = <any>error);
+  }
 }
