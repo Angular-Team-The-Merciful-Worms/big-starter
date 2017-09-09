abdd03c8a3f9dc1cc814c26167f4f45d6edeb5a1
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-09-09 10:35:51 +0300

    added newly created contacts component and route to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './home/welcome.component';
 import { ProjectModule } from './project/project.module';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: 'welcome', component: WelcomeComponent },
+      { path: 'contacts', component: ContactsComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' } // 404 to be implemented
     ]),
