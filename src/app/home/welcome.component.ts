09735fb7d2147e3f0883291d7d6cbdfa8529d283
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2017-09-10 17:19:54 +0300

    use modal and auth service

--- src/app/home/welcome.component.ts
+++ src/app/home/welcome.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { AuthService } from '../core/auth.service';
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     templateUrl: './welcome.component.html',
     styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
     public pageTitle = 'Big Starter';
+
+    constructor(private modalService: NgbModal, public auth: AuthService) { }
+
+    ngOnInit(): void {
+        console.log(this.auth)
+    }
 }
