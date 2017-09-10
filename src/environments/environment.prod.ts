05c2b5f67fa0c0aad34bd0547a6552b1b080b833
tinmanjk <tinmanjk@mail.com>
2017-09-10 15:24:27 +0300

    added firebase to prod environment settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyChHjVujuxong1-u1zJ79sJbLNUjsELFq0',
+    authDomain: 'big-starter.firebaseapp.com',
+    databaseURL: 'https://big-starter.firebaseio.com',
+    projectId: 'big-starter',
+    storageBucket: 'big-starter.appspot.com',
+    messagingSenderId: '935276327530',
+  },
 };
