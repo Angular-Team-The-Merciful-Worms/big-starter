fe7bc710ef7924ed82f309b0683feceb266f5562
Milena Sapunova <milena.sapunova@gmail.com>
2017-09-10 21:54:59 -0700

    Refactor

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -3,10 +3,12 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { TitleCasePipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { ProjectItemComponent } from './project-item/project-item.component';
 import { ProjectCategoriesComponent } from './project-categories/project-categories.component';
+import { AddProjectComponent } from './add-project/add-project.component';
 
 import { ProjectsFireService } from './projects-fire.service';
 import { ProjectService } from './projects-local.service';
@@ -16,20 +18,23 @@ import { ProjectListItemsComponent } from './project-list-items/project-list-ite
   imports: [
     SharedModule,
     RouterModule.forChild([
+      { path: 'projects/new', component: AddProjectComponent },
       { path: 'category/:category', component: ProjectsListComponent },
       { path: 'projects', component: ProjectCategoriesComponent },
       {
         path: 'projects/:id',
         component: ProjectItemComponent
       }
-    ])
+    ]),
+    ReactiveFormsModule,
   ],
   declarations: [
     ProjectsListComponent,
     ProjectItemComponent,
     ProjectCategoriesComponent,
     ProjectListItemsComponent,
-    DonateComponent
+    DonateComponent,
+    AddProjectComponent
   ],
   providers: [
     ProjectService,
