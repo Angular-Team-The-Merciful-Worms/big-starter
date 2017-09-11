62231b77cfbed87144a9e5a1f181143d3e09a1fa
Milena Sapunova <milena.sapunova@gmail.com>
2017-09-10 17:45:57 -0700

    Add validation

--- src/app/project/add-project/add-project.component.ts
+++ src/app/project/add-project/add-project.component.ts
@@ -20,10 +20,30 @@ export class AddProjectComponent implements OnInit {
     formErrors = {
         'projectname': '',
         'location': '',
-        'authorName': '',
         'targetFunds': '',
         'descirpiton': '',
-        'cateogry': '',
+    };
+
+    validationMessages = {
+        'projectname': {
+            'required': 'Project name is required.',
+            'minlength': 'Project name must be at least 4 characters long.',
+            'maxlength': 'First name cannot be more than 25 characters long.',
+        },
+        'loaction': {
+            'required': 'Location is required.',
+            'minlength': 'Location must be at least 2 characters long.',
+            'maxlength': 'Location cannot be more than 25 characters long.',
+        },
+        'descirpiton': {
+            'required': 'Description is required.',
+            'minlength': 'Description must be at least 250 characters long.',
+            'maxlength': 'Description cannot be more than 25,000 characters long.',
+        },
+        'balance': {
+            'required': 'Balance is required.',
+            'min': 'Balance should be a non negative number',
+        },
     };
 
     constructor(private authService: AuthService, private fb: FormBuilder, private uploadService: UploadService) {
@@ -44,6 +64,46 @@ export class AddProjectComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.buildForm();
+    }
+
+    buildForm(): void {
+        this.userForm = this.fb.group({
+            'projectname': ['', [
+                Validators.required,
+                Validators.minLength(4),
+                Validators.maxLength(25)
+            ]],
+            'location': ['', [
+                Validators.required,
+                Validators.minLength(2),
+                Validators.maxLength(25)
+            ]],
+            'descirpiton': ['', [
+                Validators.required,
+                Validators.minLength(250),
+                Validators.maxLength(25000)
+            ]],
+            'targetFunds': ['', [
+                Validators.required,
+                Validators.min(1000),
+            ]],
+        });
+
+        // this.userForm.valueChanges.subscribe(data => this.onValueChanged(data));
+        // this.onValueChanged(); // reset validation messages
+    }
+    updateData(): void {
+        const user = {
+            email: this.userForm.value['email'],
+            password: this.userForm.value['password'],
+            firstname: this.userForm.value['firstname'],
+            lastname: this.userForm.value['lastname'],
+            name: this.userForm.value['name'],
+            balance: this.userForm.value['balance'],
+        };
+
+        this.authService.updateUserData(user);
     }
 
     createProject() {
